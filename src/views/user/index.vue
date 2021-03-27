886a9a5789dd167321be3c955757c39c234df4b8
xiaominglin789 <xiaominglin789@gmail.com>
2021-03-27 12:29:08 +0800

    注释测试core-panel、core-slider,可使用

--- src/views/user/index.vue
+++ src/views/user/index.vue
@@ -23,7 +23,8 @@
     </com-top-bar>
     <!-- 入口 -->
     <com-entry :entries="entries" :column="5" :border="false"></com-entry>
-    <!-- 测试core-panel和core-slider组件 -->
+
+    <!-- 测试core-panel、core-slider
     <core-panel
       title="猫照"
       :showMore="true"
@@ -33,10 +34,8 @@
     >
       <img :src="userInfo.img" />
     </core-panel>
-    <!-- 水平滑动组件 -->
     <core-panel title="我的爱宠">
       <core-slider :list="myPets">
-        <!-- 想在外部传入子项的样式结构 -->
         <template v-slot:item="{ item }">
           <div class="pet-box" @click="onClickSlideItem(item)">
             <van-image
@@ -51,8 +50,7 @@
           </div>
         </template>
       </core-slider>
-    </core-panel>
-    <!-- 分割线 -->
+    </core-panel>-->
   </div>
 </template>
 
@@ -60,35 +58,31 @@
 import { defineComponent } from 'vue'
 import ComTopBar from '@/components/com-topbar.vue'
 import ComEntry from '@/components/com-entry.vue'
-import CorePanel from '@/components/core/core-panel.vue'
-import CoreSlider from '@/components/core/core-slider.vue'
 
 export default defineComponent({
   name: 'User',
   components: {
     ComTopBar,
     ComEntry,
-    CorePanel,
-    CoreSlider,
   },
   data: () => {
     return {
-      myPets: [
-        {
-          id: 1,
-          name:
-            '云养猫云养猫云养猫云养猫云养猫云养猫云养猫云养猫云养猫云养猫云养猫',
-          img: 'https://picsum.photos/64/64?random=1',
-          desctription:
-            '正宗旺财正宗旺财正宗旺财正宗旺财正宗旺财正宗旺财正宗旺财正宗旺财正宗旺财正宗旺财正宗旺财正宗旺财正宗旺财正宗旺财正宗旺财正宗旺财正宗旺财正宗旺财正宗旺财正宗旺财正宗旺财正宗旺财!',
-        },
-        {
-          id: 2,
-          name: '招财',
-          img: 'https://picsum.photos/64/64?random=2',
-          desctription: '懒猫!',
-        },
-      ],
+      // myPets: [
+      //   {
+      //     id: 1,
+      //     name:
+      //       '云养猫云养猫云养猫云养猫云养猫云养猫云养猫云养猫云养猫云养猫云养猫',
+      //     img: 'https://picsum.photos/64/64?random=1',
+      //     desctription:
+      //       '正宗旺财正宗旺财正宗旺财正宗旺财正宗旺财正宗旺财正宗旺财正宗旺财正宗旺财正宗旺财正宗旺财正宗旺财正宗旺财正宗旺财正宗旺财正宗旺财正宗旺财正宗旺财正宗旺财正宗旺财正宗旺财正宗旺财!',
+      //   },
+      //   {
+      //     id: 2,
+      //     name: '招财',
+      //     img: 'https://picsum.photos/64/64?random=2',
+      //     desctription: '懒猫!',
+      //   },
+      // ],
       entries: [
         {
           title: '领养',
@@ -117,8 +111,7 @@ export default defineComponent({
         },
       ],
       userInfo: {
-        name:
-          '云养猫云养猫云养猫云养猫云养猫云养猫云养猫云养猫云养猫云养猫云养猫',
+        name: '云养猫',
         tags: ['关注 20', '粉丝 98'],
         img: 'https://img.yzcdn.cn/vant/cat.jpeg',
       },
@@ -173,32 +166,32 @@ export default defineComponent({
   }
 
   /** core-slider 子组件样式控制 */
-  &/deep/.pet-box {
-    box-sizing: border-box;
-    width: 200px;
-    height: 120px;
-    border-radius: 10px;
-    background-color: rgb(253, 124, 38);
-    margin-right: 10px;
-    padding: 16px;
-    position: relative;
-    overflow: hidden;
-    h3 {
-      position: absolute;
-      top: 0.7rem;
-      left: 1.8rem;
-      color: #ffffff;
-      font-size: 18px;
-      @include ellipsis;
-    }
-    p {
-      color: #ffffff;
-      font-size: 14px;
-      @include ellipsis(2);
-    }
-  }
-  &/deep/ul > li:first-child .pet-box {
-    background-color: $app-bg-color-base;
-  }
+  // &/deep/.pet-box {
+  //   box-sizing: border-box;
+  //   width: 200px;
+  //   height: 120px;
+  //   border-radius: 10px;
+  //   background-color: rgb(253, 124, 38);
+  //   margin-right: 10px;
+  //   padding: 16px;
+  //   position: relative;
+  //   overflow: hidden;
+  //   h3 {
+  //     position: absolute;
+  //     top: 0.7rem;
+  //     left: 1.8rem;
+  //     color: #ffffff;
+  //     font-size: 18px;
+  //     @include ellipsis;
+  //   }
+  //   p {
+  //     color: #ffffff;
+  //     font-size: 14px;
+  //     @include ellipsis(2);
+  //   }
+  // }
+  // &/deep/ul > li:first-child .pet-box {
+  //   background-color: $app-bg-color-base;
+  // }
 }
 </style>
